9bf0216b3769a10e564e6965b811dbbc4cf18f81
Danilo Bustos <danilobustos@MacBook-Pro-de-Danilo.local>
2014-04-07 22:11:41 -0300

    Added some examples about search and tweet stream

--- src/tweet_filter/TweetFilter.java
+++ src/tweet_filter/TweetFilter.java
@@ -1,6 +1,15 @@
 package tweet_filter;
 
+import java.util.List;
+
 import twitter4j.FilterQuery;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.StatusListener;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 import twitter4j.auth.AccessToken;
@@ -9,6 +18,13 @@ public class TweetFilter {
 
 	public static void main(String[] args) {
 		
+		TweetFilter tweetFilter = new TweetFilter();
+		tweetFilter.startSearchTweets();
+		
+        	
+	}
+	
+	public void startStream(){
 		// Getting the instance associated with the configuration
 		TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
 		
@@ -18,10 +34,48 @@ public class TweetFilter {
 		
 		twitterStream.setOAuthAccessToken(accessToken);
 		
-		String keywords[] = {"Colo Colo", "Universidad de Chile"};
+		String keywords[] = {"Universidad de Santiago", "Usach", "UdeSantiago"};
 		FilterQuery filter = new FilterQuery();
         filter.track(keywords);
         
+        StatusListener listener = new TweetListener();
+        twitterStream.addListener(listener);
+        
+        twitterStream.filter(filter);
+	}
+	
+	public void startSearchTweets(){
+		Twitter twitter = TwitterFactory.getSingleton();
+		
+		twitter.setOAuthConsumer(TwitterConnection.getInstance().getConsumerKey(), TwitterConnection.getInstance().getConsumerSecret());
+		AccessToken accessToken = new AccessToken(TwitterConnection.getInstance().getAccessToken() ,TwitterConnection.getInstance().getAccessTokenSecret());
+		twitter.setOAuthAccessToken(accessToken);
+		
+		Query query = new Query("Usach");
+		query.count(100);
+		query.since("2010-01-01");
+		query.until("2010-04-01");
+		
+		QueryResult result;
+
+		System.out.println("Hola");
+		
+		try {
+			result = twitter.search(query);
+			List<Status> tweets = result.getTweets();
+            int num_tweet = 1;
+			for (Status tweet : tweets) {
+                System.out.println(num_tweet + "-   @" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+                num_tweet++;
+			}
+			
+		} catch (TwitterException e) {
+            e.printStackTrace();
+            System.out.println("Failed to search tweets: " + e.getMessage());
+            System.exit(-1);
+		}
+	    
+
 		
 	}
 
--- src/tweet_filter/TweetListener.java
+++ src/tweet_filter/TweetListener.java
@@ -1,9 +1,11 @@
 package tweet_filter;
 
+import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
 
-public class TweetListener {
+public class TweetListener implements StatusListener {
 	
 	public TweetListener(){
 	
@@ -26,5 +28,15 @@ public class TweetListener {
 	public void onException(Exception ex) {
         ex.printStackTrace();
     }
+
+	public void onScrubGeo(long arg0, long arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStallWarning(StallWarning arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
