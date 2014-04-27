59a84aeae12039ab9967cc6c644793ccf5e32f8e
Danilo <dbustos10@gmail.com>
2014-04-26 21:49:55 -0300

    Stream connected to mongolab

--- src/tweet_filter/TweetListener.java
+++ src/tweet_filter/TweetListener.java
@@ -1,21 +1,46 @@
 package tweet_filter;
 
+import java.net.UnknownHostException;
+
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 
 public class TweetListener implements StatusListener {
 	
 	// Database object
 	MongoDB mongo;
+	
+	// Database in Mongolab
+	MongoClient mongolab;
+	DB db_mongolab;
+	DBCollection coll_mongolab = null;
 
 	
 	
-	public TweetListener(MongoDB _mongo){
+	public TweetListener(MongoDB _mongo, String host_database, int port_database, String db_name, String collection_name){
 		mongo = _mongo;
+		try {
+			mongolab = new MongoClient(host_database, port_database);
+			db_mongolab = mongolab.getDB(db_name);
+			coll_mongolab = db_mongolab.getCollection(collection_name);
+			boolean auth = db_mongolab.authenticate("tecweb_user", "tecweb12345".toCharArray());
+			if (auth) {		 
+				System.out.println("Login is successful!");
+			} else {
+				System.out.println("Login is failed!");
+			}	
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void onStatus(Status status) {
@@ -24,7 +49,7 @@ public class TweetListener implements StatusListener {
         // Parsing and saving tweets filtered
         DBObject tweet = mongo.parsingTweet(null, status);        
         // Saving tweet
-        mongo.saveTweet(tweet);
+        coll_mongolab.insert(tweet);
     }
 	
 	public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
